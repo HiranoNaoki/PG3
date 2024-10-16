f91df4f760ba33f3f93d1fb7e4ebd6398310969b
HiranoNaoki <146906525+HiranoNaoki@users.noreply.github.com>
2024-10-16 14:42:36 +0900

    OK

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include<iostream>
 #include<windows.h>
 
+
+
+int Recursive(int n) {
+	if (n<= 1)
+	{
+		return 100;
+
+	}
+	return ( Recursive(n-1)*2-50);
+}
+
 int main() {
-	SetConsoleOutputCP(65001);
-	char str[] = "ソフトバンク";
-	printf("%s", str);
+	
+
+
+	
+	int result;
+
+	for (int n = 1; n < 10; n++)
+	{
+		result = Recursive(n);
+		printf("%d時間後の給料　= %d\n", n, result);
+	}
+	
 	return 0;
 }
