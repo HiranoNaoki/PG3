ca03edb7e02985f6b6b704874f6c690f45927d91
HiranoNaoki <146906525+HiranoNaoki@users.noreply.github.com>
2025-02-04 10:51:00 +0900

    owari

--- main.cpp
+++ main.cpp
@@ -2,47 +2,50 @@
 #include<windows.h>
 
 
-typedef void (*PFunc)(int*);
+void setTimeout(int second) {
 
-void DispResult(int* s) {
-	printf("%d秒待って実行されたよ\n", *s);
-
-}
-
-void setTimeout(PFunc p, int second) {
+	
 	Sleep(second * 1000);
-
-	p(&second);
+	
 }
 
-int main() {
+auto Funciton = [=](int answer) {
 	
-	int dice;
-	int n;
-
-	printf("サイコロふる\n");
-
-	srand((unsigned int)time(NULL));
-
-	dice = rand() % 6 + 1;
-
-	printf("半か丁か\n");
-	printf("1が半、2が丁\n");
-
-	scanf_s("%d", &n);
-
-	setTimeout(DispResult, 3);
-
-	if ((dice == 1 && n == 2) || (dice == 2 && n == 1) || (dice == 3 && n == 2) || (dice == 4 && n == 1) || (dice == 5 && n == 2) || (dice == 6 && n == 1)) {
-
-		printf("不整脈\n");
-	}
-	else
-	{
-		printf("正常\n");
-	}
+	int diceNumber = rand();
 
 	
+	if (answer <= 1) {
+		if (answer == diceNumber % 2) {
+			return printf("%d,正解\n", diceNumber % 6);
+		}
+		else if (answer != diceNumber % 2) {
+			return printf("%d,不正解\n", diceNumber % 6);
+		}
+	}
+	else {
+		return printf("指定の数字以外が入力されました");
+	}
 
+
+	return printf("end");
+};
+
+int main(int argc, const char* argv[]) {
+
+	unsigned int randTime = (unsigned int)time(nullptr);
+	srand(randTime);
+
+	int answer;
+
+	
+	printf("半(1)か丁(0)か\n");
+	scanf_s("%d", &answer);
+
+	printf("start\n");
+
+	setTimeout(5);
+
+	Funciton(answer);
+	
 	return 0;
 }
