353ec6a80273c045e611d70baffc619390b9ed66
HiranoNaoki <146906525+HiranoNaoki@users.noreply.github.com>
2025-02-04 12:09:03 +0900

    06_02

--- main.cpp
+++ main.cpp
@@ -5,17 +5,24 @@
 #include<stdlib.h>
 #include"Template.h"
 #include"Animal.h"
+#include <thread>
+
+using namespace std;
+
+void PrintThread(uint32_t num) {
+
+	cout << "thread" << num << endl;
+}
 
 int main() {
 
-    Animal* animal1 = new Dog();  
-    Animal* animal2 = new Cat();  
-
-    animal1->sound();  
-    animal2->sound();  
-
-    delete animal1;
-    delete animal2;
+ 
+	thread t1(PrintThread, 1);
+	t1.join();
+	thread t2(PrintThread, 2);
+	t2.join();
+	thread t3(PrintThread, 3);
+	t3.join();
    
     return 0;
 }
